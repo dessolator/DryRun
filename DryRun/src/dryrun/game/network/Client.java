ede18523c387d9ccb8bf9b26c76d432a487489f0
Goran <Goran@GoranLT>
2013-12-07 01:51:06 +0100

    updated constants

--- DryRun/src/dryrun/game/network/Client.java
+++ DryRun/src/dryrun/game/network/Client.java
@@ -1,7 +1,9 @@
 package dryrun.game.network;
 
+import java.io.IOException;
 import java.net.*;
 import java.util.*;
+import static dryrun.game.network.NetConstants.*;
 
 public class Client implements NetFramework {
 	private DatagramSocket udpSocket;
@@ -16,8 +18,29 @@ public class Client implements NetFramework {
 		
 	}
 	
-	public ArrayList<Server> findServers() {
-		
+	public ArrayList<InetAddress> findServers() {
+		byte [] broadcast = "find_server".getBytes();
+		try {
+			DatagramPacket data = new DatagramPacket(broadcast,broadcast.length,InetAddress.getByName("255.255.255.255"),UDPPORT);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		ArrayList<InetAddress> servers = new ArrayList<InetAddress> ();
+		long startTime = System.currentTimeMillis();
+		while (System.currentTimeMillis()-startTime<3000000000l) {
+			byte [] receiveMessage = new byte[20];
+			DatagramPacket receivePacket = new DatagramPacket(receiveMessage,receiveMessage.length);
+			try {
+				udpSocket.receive(receivePacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			String server = new String(receivePacket.getData()).trim();
+			if (server.equals("find_server_reply")) {
+				servers.add(receivePacket.getAddress());
+			}
+		}
+		return servers;		
 	}
 	
 	public void send(GameStatePacket gState) {
--- DryRun/src/dryrun/game/network/NetConstants.java
+++ DryRun/src/dryrun/game/network/NetConstants.java
@@ -3,4 +3,6 @@ package dryrun.game.network;
 public class NetConstants {
 	static final int TCPPORT = 50000;
 	static final int UDPPORT = 50001;
+	static final String FIND_SERVER = "find_server";
+	static final String FIND_SERVER_R = "find_server_reply";
 }
