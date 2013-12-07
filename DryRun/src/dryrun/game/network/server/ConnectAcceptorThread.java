f339f555e3cd9d48055f27fa5b81118b20e65c09
Nikola <dnn799@gmail.com>
2013-12-07 21:58:38 +0100

    - Server work

---
+++ DryRun/src/dryrun/game/network/server/ConnectAcceptorThread.java
@@ -0,0 +1,5 @@
+package dryrun.game.network.server;
+
+public class ConnectAcceptorThread {
+
+}
---
+++ DryRun/src/dryrun/game/network/server/RefreshReplyThread.java
@@ -0,0 +1,38 @@
+package dryrun.game.network.server;
+
+import static dryrun.game.network.NetConstants.FIND_SERVER;
+import static dryrun.game.network.NetConstants.FIND_SERVER_R;
+
+import java.io.IOException;
+import java.net.*;
+
+public class RefreshReplyThread extends Thread {
+	private DatagramSocket myUdpSocket;
+	
+	
+	public RefreshReplyThread(DatagramSocket UdpSocket){myUdpSocket=UdpSocket;}
+	
+	
+	void run(){
+		byte x[] = new byte [100];
+		while(!interrupted()){
+			DatagramPacket receive = new DatagramPacket(x, 100);
+			try {
+				myUdpSocket.receive(receive);
+			} catch (IOException e) {e.printStackTrace(); break;}
+			String s = new String(receive.getData()).trim();
+			if(s==FIND_SERVER){//TODO string.equals
+				s=FIND_SERVER_R;
+				x=s.getBytes();
+				DatagramPacket reply = new DatagramPacket(x, x.length, receive.getAddress(), receive.getPort());
+				try {
+					myUdpSocket.send(reply);
+				} catch (IOException e) { e.printStackTrace(); break;}
+			}
+		}
+		
+	}
+	
+	
+
+}
