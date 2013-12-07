250b8391f3599d53f2daaff7dabad91b624706f7
Wolf <Vuk@Vuk-PC>
2013-12-07 21:44:33 +0100

    Timer fixed again

--- DryRun/src/dryrun/game/objects/bonus/Timer.java
+++ DryRun/src/dryrun/game/objects/bonus/Timer.java
@@ -7,13 +7,10 @@ import java.util.ArrayList;
 
 public class Timer {
 	private static final int BONUS_DURATION = 10;
-	private static ArrayList<Timer> timers;
 	long time;//vreme isteka bonusa i nanosekundama
 	private Bonus myBonus;//bonus na koji utice timer
 	//inicijalizacija liste tajmera
-	static{
-		timers= new ArrayList<Timer>();
-	}
+
 
 	//kostruktor koji uzima 2 parametra 
 	public Timer(int seconds,Bonus b){
@@ -25,21 +22,6 @@ public class Timer {
 	//proveri da li je zavrsen timer
 	public boolean isPassed(){
 		return (System.nanoTime()>time?true:false);
-	}
-	//dohvatamo private static polje liste
-	private static ArrayList<Timer> getTimers() {
-		return timers;
-	}
-	
-	//izbacivanje timera zadatkog bonusa iz liste
-	private static void removeTypedTimer(Bonus bonus) { 
-		for (int i=0; i<Timer.getTimers().size();i++) {
-			if(Timer.getTimers().get(i).myBonus==bonus){
-				Timer.getTimers().remove(i);
-				i--;
-			}
-		}
-	
 	}
 
 	public Bonus getMyBonus() {
