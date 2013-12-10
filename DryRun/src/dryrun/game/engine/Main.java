bba06e011d307afc751cba248bfb3a431e3c721b
Wolf <Vuk@Vuk-PC>
2013-12-10 23:53:29 +0100

    -vuk push main

--- DryRun/src/dryrun/game/engine/Main.java
+++ DryRun/src/dryrun/game/engine/Main.java
@@ -8,11 +8,13 @@ public class Main {
 	public static int i=0;
 
 	public static void main(String[] args) {
+		//asfojasofghjasdgf'pasf
 		Engine.init();
 		LoadTex.init();
 		Game.initGame();
 		Game.startGame();		
 		Engine.kill();
+		//askdfjaspjfpasf
 	}
 
 }
