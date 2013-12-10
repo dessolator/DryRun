f59c17eeb96905414efe780f892a6b7bbefb5142
Goran <Goran@GoranLT>
2013-12-10 20:20:21 +0100

    ggg

--- DryRun/src/dryrun/game/network/GameStatePacket.java
+++ DryRun/src/dryrun/game/network/GameStatePacket.java
@@ -5,18 +5,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 
 import dryrun.game.common.GameObjectValues;
 import static dryrun.game.network.NetConstants.*;
 
-public class GameStatePacket  {
+public class GameStatePacket implements Serializable  {
 //	private ArrayList<GameObjectValues> myObjects=new ArrayList<GameObjectValues>();
 	private GameObjectValues [] myObjects;
 	private int i=0;
 	
 	public GameStatePacket(){
-		myObjects = new GameObjectValues[MAX_PLAYERS-1];
+		myObjects = new GameObjectValues[1];
 	}
 	
 	public void put(GameObjectValues gov){
@@ -52,15 +53,12 @@ public class GameStatePacket  {
 			ByteArrayInputStream bais = new ByteArrayInputStream(x);
 			ObjectInputStream ois;
 			ois = new ObjectInputStream(bais);
-			p = getPacket(ois);
+			p = (GameStatePacket) ois.readObject();
 		} catch (IOException e) {e.printStackTrace();} catch (ClassNotFoundException e){e.printStackTrace();}
 		return p;
 		
 	}
 	
-	protected  static GameStatePacket getPacket(ObjectInputStream ois) throws ClassNotFoundException, IOException {
-		return (GameStatePacket) ois.readObject();
-	}
 	
 	
 	
