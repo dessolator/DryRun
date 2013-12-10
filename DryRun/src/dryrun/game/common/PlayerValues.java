085324c9aa0812554f1c914cd67d6132f0a2eb89
Nikola <dnn799@gmail.com>
2013-12-10 15:16:25 +0100

    -bonus

--- DryRun/src/dryrun/game/common/PlayerValues.java
+++ DryRun/src/dryrun/game/common/PlayerValues.java
@@ -1,5 +1,14 @@
 package dryrun.game.common;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import dryrun.game.objects.GameObject;
+
 public class PlayerValues extends GameObjectValues {
+	private String name;
+	protected List<GameObject> myObjects=Collections.synchronizedList(new ArrayList<GameObject>());
 	
+	public List<GameObject> getObjList(){return myObjects;}
 }
