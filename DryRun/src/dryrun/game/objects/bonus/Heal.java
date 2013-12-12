37b915a5aefafc79e93fd8c21f82c7dfd20bac97
Wolf <Vuk@Vuk-PC>
2013-12-12 23:57:25 +0100

    bonuses almost complete

--- DryRun/src/dryrun/game/objects/bonus/Heal.java
+++ DryRun/src/dryrun/game/objects/bonus/Heal.java
@@ -3,13 +3,17 @@ package dryrun.game.objects.bonus;
 import org.jbox2d.common.Vec2;
 import org.newdawn.slick.opengl.Texture;
 
+import dryrun.game.engine.Tex;
+import dryrun.game.engine.TextureHolder;
 import dryrun.game.objects.Player;
 
+import static dryrun.game.engine.LoadTex.heal;
 public class Heal extends Bonus {
 
 	public Heal(float x, float y, float dimx, float dimy, int myInd,
 			Vec2 position) {
-		super(x, y, dimx, dimy, myInd, position);	
+		super(x, y, dimx, dimy, myInd, position);
+		this.holder = new TextureHolder(heal, new Tex(0f, 0f, 1f, 1f));
 	}
 
 	@Override
