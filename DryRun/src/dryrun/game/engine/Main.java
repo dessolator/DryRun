3353eb06af3f27bbbc67890bc18e8a7d03bea58d
Nikola <dnn799@gmail.com>
2013-12-12 17:33:32 +0100

    cleanup

--- DryRun/src/dryrun/game/engine/Main.java
+++ DryRun/src/dryrun/game/engine/Main.java
@@ -11,13 +11,13 @@ public class Main {
 	public static int i=0;
 
 	public static void main(String[] args) {
-	//	SEngine.init();
-//		SEngine.getSoundSystem().quickPlay(false, "splashsound.ogg", false, 0, 0, 0, 0, 0);
+		SEngine.init();
+		SEngine.getSoundSystem().quickPlay(false, "splashsound.ogg", false, 0, 0, 0, 0, 0);
 		Engine.init();
 		LoadTex.init();
 		Game.initGame();
 		Game.startGame();
-	//	SEngine.getSoundSystem().cleanup();
+		SEngine.getSoundSystem().cleanup();
 		Engine.kill();
 	}
 
