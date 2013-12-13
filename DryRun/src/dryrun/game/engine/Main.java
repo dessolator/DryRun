8a620b04d998f2853fe00f915981965ef71181d6
Nikola <dnn799@gmail.com>
2013-12-13 02:55:24 +0100

    asdf

--- DryRun/src/dryrun/game/engine/Main.java
+++ DryRun/src/dryrun/game/engine/Main.java
@@ -14,10 +14,14 @@ public class Main {
 		SEngine.init();
 		Engine.init();
 		//SEngine.getSoundSystem().quickPlay(false, "splashsound.ogg", false, 0, 0, 0, 0, 0);
-		SEngine.getSoundSystem().newStreamingSource(false,"engine", "engineidleloop.ogg", false, 0, 0, 0, 0, 0);
-		SEngine.getSoundSystem().queueSound("engine", "engineidle.ogg");
-		SEngine.getSoundSystem().setLooping("engine", true);
-		SEngine.getSoundSystem().play("engine");
+		SEngine.getSoundSystem().newStreamingSource(false,"engine", "engineidleloop.ogg", true, 0, 0, 0, 0, 0);
+
+	//	SEngine.getSoundSystem().play("engine");
+	//	SEngine.getSoundSystem().fadeOutIn( "engine", "engineidleloop.ogg", 300, 300 );
+	//	SEngine.getSoundSystem().checkFadeVolumes();
+	//	SEngine.getSoundSystem().queueSound("engine", "engineidleloop.ogg");
+	//	SEngine.getSoundSystem().setLooping("engine", true);
+	//	SEngine.getSoundSystem().play("engine");
 		
 		LoadTex.init();
 		Game.initGame();
